a8de7f6dd7d301b8e43021178496852ec6b76ff2
Stephan <stepandegtsiaryk@syndi-g.com>
2025-11-28 11:31:47 +0300

    Initial Commit

---
+++ MixFruits/MixFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixFruitsApp.swift
+//  MixFruits
+//
+//  Created by Stepan Degtsiaryk on 28.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MixFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
