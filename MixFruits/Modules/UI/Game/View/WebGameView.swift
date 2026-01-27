3aaddb60f6394c2df561eb0e01cddd44d0b431d1
Stephan <stepandegtsiaryk@syndi-g.com>
2026-01-27 15:55:11 +0300

    add framework

--- MixFruits/Modules/UI/Game/View/WebGameView.swift
+++ MixFruits/Modules/UI/Game/View/WebGameView.swift
@@ -12,13 +12,34 @@ struct WebGameView: View {
     @StateObject private var viewModel = WebGameViewModel()
 
     var body: some View {
-        NavigationStack {
+        if #available(iOS 16.0, *) {
+            NavigationStack {
+                VStack(spacing: 8) {
+                    ZStack {
+                        Color.brown.ignoresSafeArea()
+                        
+                        WebView(viewModel: viewModel)
+                        
+                        if viewModel.isLoading {
+                            Color.brown.ignoresSafeArea()
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle())
+                        }
+                    }
+                }
+                .navigationBarTitleDisplayMode(.large)
+                .alert(item: $viewModel.errorMessage) { msg in
+                    Alert(title: Text("Error"), message: Text(msg.message), dismissButton: .default(Text("OK")))
+                }
+            }
+        } else {
+            // Fallback on earlier versions
             VStack(spacing: 8) {
                 ZStack {
                     Color.brown.ignoresSafeArea()
-
+                    
                     WebView(viewModel: viewModel)
-
+                    
                     if viewModel.isLoading {
                         Color.brown.ignoresSafeArea()
                         ProgressView()
--- Podfile
+++ Podfile
@@ -1,21 +1,33 @@
 # Uncomment the next line to define a global platform for your project
- platform :ios, '15.0'
+platform :ios, '15.0'
 
 target 'MixFruits' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-
+  
   # Pods for MixFruits
-  pod 'Firebase/Core'
-  pod 'Firebase/Messaging'
-  pod 'AppsFlyerFramework'
+  pod 'Alamofire', '= 5.8'
+  pod 'AppsFlyerFramework', '= 6.12'
+  pod 'Firebase/Core', '12.7.0'
+  pod 'Firebase/Messaging', '12.7.0'
+  
+end
 
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.0'
+      config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'YES'
+    end
+  end
 end
 
 target 'notifications' do
   use_frameworks!
-
-  pod 'Firebase/Core'
-  pod 'Firebase/Messaging'
-  pod 'AppsFlyerFramework'
+  
+  pod 'Alamofire', '= 5.8'
+  pod 'AppsFlyerFramework', '= 6.12'
+  pod 'Firebase/Core', '12.7.0'
+  pod 'Firebase/Messaging', '12.7.0'
+  
 end
